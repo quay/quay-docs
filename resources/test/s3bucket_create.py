77596f792fc56246045de075dbf0015a5a7b3da7
Steven Smith <77019920+stevsmit@users.noreply.github.com>
2025-02-25 10:47:06 -0500

    Adds tag API commands (#1226)
    Co-authored-by: Steven Smith <stevsmit@stevsmit-thinkpadt14gen4.remote.csb>

--- resources/test/s3bucket_create.py
+++ resources/test/s3bucket_create.py
@@ -1,11 +1,11 @@
-# From: https://access.redhat.com/documentation/en-us/red_hat_ceph_storage/3/html/object_gateway_guide_for_red_hat_enterprise_linux/configuration#creating_a_literal_radosgw_literal_user_for_s3_access
+# From: https://access.redhat.com/documentation/en-us/red_hat_ceph_storage/{producty}/html/object_gateway_guide_for_red_hat_enterprise_linux/configuration#creating_a_literal_radosgw_literal_user_for_s3_access
 import boto
 import boto.s3.connection
 
 rdgw_hostname = "quay.quaylab.lan"
 rdgw_port = 8880
 ##
-# Fill this in after running: https://access.redhat.com/documentation/en-us/red_hat_ceph_storage/3/html/object_gateway_guide_for_red_hat_enterprise_linux/administration_cli#create_a_user
+# Fill this in after running: https://access.redhat.com/documentation/en-us/red_hat_ceph_storage/{producty}/html/object_gateway_guide_for_red_hat_enterprise_linux/administration_cli#create_a_user
 # $ radosgw-admin user create --uid=janedoe --display-name="Jane Doe" --email=jane@example.com
 ##
 access_key = $access
